d9f86ca18a060e178d4c804a7100b1d182206ab5
ladylaura22 <lady.olmos@est.iudigital.edu.co>
2025-06-10 22:40:17 -0500

    Creo la clase animal, solo con los atributos nombre tipo y género, se crea el cosntructor. También se crean los Getters y Setters

---
+++ src/Animal.java
@@ -0,0 +1,37 @@
+public class Animal  {
+    private String nombre;
+    private String tipo; // "terrestre", "aéreo", "acuático"
+    private String genero; // "macho", "hembra"
+    // Constructor de la clase Animal
+    // Aqui se crea la clase Animal con sus atributos
+    public Animal(String nombre, String tipo, String genero) {
+        this.nombre = nombre;
+        this.tipo = tipo;
+        this.genero = genero;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+}
+
